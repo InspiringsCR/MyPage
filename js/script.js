7f24231b098371f60c5dc9d62114d6c7bd43345a
Gabriel Granados Ureña <gabrig1404@gmail.com>
2023-06-13 11:09:15 -0600

    Refresh Data

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
   })(jQuery);
 
   // input text for typing animation
-  $("#holder").writeText("WEB DESIGNER + JR DEVELOPER");
+  $("#holder").writeText("WEB DESIGNER + FRONT-END DEVELOPER");
 
   // initialize wow.js
   new WOW().init();
